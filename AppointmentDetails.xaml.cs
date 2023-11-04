ac9d6ba98cb93ea21d850309021d2faaf1a592bb
Shunzi Yao <2395028@edu.vaniercollege.qc.ca>
2023-11-03 23:41:52 -0400

    added appointments page

---
+++ AppointmentDetails.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OCMS
+{
+    /// <summary>
+    /// Interaction logic for AppointmentDetails.xaml
+    /// </summary>
+    public partial class AppointmentDetails : Window
+    {
+        private NpgsqlConnection con;
+
+        public AppointmentDetails()
+        {
+            InitializeComponent();
+            DatabaseHelper dbHelper = new DatabaseHelper();
+            con = dbHelper.GetConnection();
+        }
+
+        public AppointmentDetails(DataRow selectedRow)
+        {
+            InitializeComponent();
+            DatabaseHelper dbHelper = new DatabaseHelper();
+            con = dbHelper.GetConnection();
+            //FillAppointmentDetails(selectedRow);
+        }
+    }
+}
